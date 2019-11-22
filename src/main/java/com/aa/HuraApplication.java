fa678e0678bafafcfe4e6402f83f63df66d0cebe
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-11-22 14:18:17 +0000

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/aa/HuraApplication.java
@@ -0,0 +1,12 @@
+package com.aa;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuraApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(HuraApplication.class, args);
+	}
+
+}
